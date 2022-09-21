e7fa5c5a6e27e7365200db35934b6dfd129b43fa
zWolv <samoht684@gmail.com>
2022-09-21 15:32:57 +0200

    update 1

---
+++ PONG/Racket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PONG
+{
+    public class Racket
+    {
+       
+       public Racket(_x1,_y1,_x2,_y2,_width,_height)
+        {
+            int _x1 = x1;
+            int _y1 = y1;
+            int _x2 = x2;
+            int _y2 = y2;
+            int _width = width;
+            int _height = height;
+        }
+
+       public void movement(x1,x2) {
+            
+       }
+
+        public void Draw()
+        {
+           
+        }
+
+
+}
