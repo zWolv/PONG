2eb6c75cc6759672ba197d18c9cba54521aa8752
Thomas <samoht684@gmail.com>
2022-09-26 19:50:25 +0200

    horizontal players - sprite tekent niet

--- PONG/Racket.cs
+++ PONG/Racket.cs
@@ -14,35 +14,73 @@ namespace PONG
         public Texture2D _sprite;
         private Vector2 spriteOrigin;
         private Vector2 _pos;
-        Keys player_up;
-        Keys player_down;
-        public bool intersect;
+        Keys player_up_right;
+        Keys player_down_left;
 
-            public Racket(int _x1,int _y1, Keys _player_up, Keys _player_down)
+        public bool intersect;
+        public enum direction {
+        horizontal,
+        vertical
+        }
+        public direction richting;
+        int width;
+        int height;
+
+            public Racket(int _x1,int _y1, Keys _player_up_right, Keys _player_down_left, direction _richting, int _screenWidth, int _screenHeight)
             {
                 x1 = _x1;
                 y1 = _y1;
-                player_up = _player_up;
-                player_down = _player_down;
+                player_up_right = _player_up_right;
+                player_down_left = _player_down_left;
+                richting = _richting;
+                height = _screenHeight;
+                width = _screenWidth;
+                
             }
 
             public void movement() 
-            {
+            {   
                 KeyboardState state = Keyboard.GetState();
 
-                if(_pos.Y <= 383) { 
-                    if(state.IsKeyDown(player_down))
-                    {
+
+                if(richting == direction.vertical)
+                {
+                    if(_pos.Y <= height - 116) 
+                    { 
+                        if(state.IsKeyDown(player_down_left))
+                        {
                         _pos.Y += 5;
+                        }
                     }
+
+                    if(_pos.Y >= height - height + 1) 
+                    {
+                        if (state.IsKeyDown(player_up_right))
+                        {   
+                            _pos.Y -= 5;
+                        }           
+                    }   
                 }
 
-                if(_pos.Y >= 1) {
-                    if (state.IsKeyDown(player_up))
+                if(richting == direction.horizontal)
+                {
+                    if(_pos.X <= width - 54)
                     {
-                        _pos.Y -= 5;
-                    }           
-                }   
+                        if(state.IsKeyDown(player_up_right)) 
+                        { 
+                            _pos.X += 5;
+                        }
+                    }
+
+                    if(_pos.X <= width - width + 1) 
+                    { 
+                        if(state.IsKeyDown(player_down_left))
+                        {
+                            _pos.X -= 5;
+                        }
+                    }
+                    
+                }  
             }
        
             public Rectangle hitbox
@@ -55,12 +93,12 @@ namespace PONG
                     }
             }
         
-            public void LoadContent(ContentManager content)
-            {
+            public void LoadContent(ContentManager  content)
+            {   
                 _sprite = content.Load<Texture2D>("batje");
                 spriteOrigin = new Vector2(_sprite.Width / 2, _sprite.Height / 2);
-                _pos = new Vector2(x1, y1);
-                _pos = _pos - spriteOrigin;
+                    _pos = new Vector2(x1, y1);
+                    _pos = _pos - spriteOrigin;
             }
 
             public void Update(Rectangle bal)
@@ -79,7 +117,14 @@ namespace PONG
 
             public void Draw(SpriteBatch _spriteBatch)
             {
-                _spriteBatch.Draw(_sprite,_pos, Color.White);
+                if(richting == direction.vertical)
+                {
+                    _spriteBatch.Draw(_sprite,_pos,Color.White);
+                } else if(richting == direction.horizontal)
+                {
+                    _spriteBatch.Draw(_sprite,_pos,null,Color.White,2,new Vector2(0),new Vector2(0),SpriteEffects.None,0f);
+                }
+                
 
             }
     } 
